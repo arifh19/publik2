2c894085c731082facfb813d008e1c465201319c
cacadosman <wetmanz23@gmail.com>
2018-12-19 11:42:58 +0700

    a

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=172.100.0.2;dbname=publikbersama',
     'username' => 'root',
-    'password' => '',
+    'password' => 'komatikugm321',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
